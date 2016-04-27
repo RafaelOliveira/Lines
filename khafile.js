5c1a4458515ed43e3280b6ab7a5c02bbdcd75e7a
Rafael Oliveira <rafah.so@gmail.com>
2016-04-27 00:54:26 -0300

    Changed the android package

--- khafile.js
+++ khafile.js
@@ -3,7 +3,10 @@ var project = new Project('Lines');
 project.addSources('Sources');
 project.addLibrary('Micro');
 
+project.targetOptions.android_native.package = 'com.sudoestegames.lines';
 project.targetOptions.android_native.screenOrientation = 'landscape';
+
+project.targetOptions.android.package = 'com.sudoestegames.lines';
 project.targetOptions.android.screenOrientation = 'landscape';
 
 return project;
